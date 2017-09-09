3595cfde3b157e1c2c8dd15587da4fecf72bbe99
Jaheem Finnie <jaheem.finnie@hgs.hiddengeniusproject.org>
2017-09-09 11:54:37 -0700

    Carrier Wave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,14 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
   config.root = Rails.root.join('tmp')
-  config.cache_dir = 'carrierwave' 
+  config.cache_dir = 'carrierwave'
     config.fog_credentials = {
       provider:              'AWS', # required
-      aws_access_key_id:     Rails.application.secrets.aws_access_key_id, # required
-      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key, # required
-      region:                'us-east-1', # optional, defaults to 'us-east-1'
+      aws_access_key_id:     => ENV['S3_ACCESS_KEY'], # required
+      aws_secret_access_key: => ENV['S3_SECRET_KEY'], # required
+      region:                'us-west-1', # optional, defaults to 'us-east-1'
     }
-    config.fog_directory  = 'socify' # required
+    config.fog_directory  = ENV['S3_Bucket'] # required
     config.fog_public     = true # optional, defaults to true
     config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
   end
